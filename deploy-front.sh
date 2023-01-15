8f7251f53a71e544bba62e69d3429c44703b87ea
a.rogov <a.rogov@tinkoff.ru>
2023-01-15 16:48:41 +0500

    DZ EXAM

---
+++ deploy-front.sh
@@ -0,0 +1,2 @@
+aws --endpoint-url=https://storage.yandexcloud.net/ s3 cp ./frontend/index.html s3://arogov/index.html
+aws --endpoint-url=https://storage.yandexcloud.net/ s3 cp ./frontend/js/main.js s3://arogov/js/main.js
